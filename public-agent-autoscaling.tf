074bc330206e4db6dac49bdc8ac136da1b1950c4
Alex Raju <alex@qburst.com>
2016-08-18 08:34:36 +0530

    Attach ELB with ASG

--- public-agent-autoscaling.tf
+++ public-agent-autoscaling.tf
@@ -18,15 +18,23 @@ resource "aws_launch_configuration" "dcos_public_agent_lc" {
 }
 
 resource "aws_autoscaling_group" "dcos_public_agent_asg" {
-  availability_zones = ["${var.aws_region}a"]
   name = "${var.pre_tag}-Public-Agent-AS-group-${var.post_tag}"
+
   max_size = "${var.public_agent_asg_max_size}"
   min_size = "${var.public_agent_asg_min_size}"
   desired_capacity = "${var.public_agent_asg_desired_capacity}"
+
   health_check_type = "${var.public_agent_asg_health_check_type}"
   health_check_grace_period = "${var.public_agent_asg_health_check_grace_period}"
+
   launch_configuration = "${aws_launch_configuration.dcos_public_agent_lc.name}"
+
+  availability_zones = ["${var.aws_region}a"]
   vpc_zone_identifier = ["${aws_subnet.availability-zone-private.id}"]
+
+  load_balancers = ["${aws_elb.tyk.name}"]
+
+  force_delete = true
 
   tag {
     key = "Name"
