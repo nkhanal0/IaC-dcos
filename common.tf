6f3b7075598722bb9b682162f02504bc740dd875
Alex Raju <alex@qburst.com>
2016-10-17 23:46:25 +0530

    ALB name formatting

--- common.tf
+++ common.tf
@@ -1,5 +1,7 @@
 resource "null_resource" "alias" {
   triggers = {
-    s3_bucket_name = "${replace(lower(var.pre_tag), "/[^0-9a-z-]/","")}"
+    s3_bucket_name = "${replace(lower(var.pre_tag), "/[^0-9a-z-]/","")}-${replace(lower(var.post_tag), "/[^0-9a-z-]/","")}"
+    lb_pre_tag = "${replace(replace(var.pre_tag, "/[^0-9a-zA-Z-]/",""), "/^(.{14}).*/","$1")}"
+    lb_post_tag = "${replace(replace(var.post_tag, "/[^0-9a-zA-Z-]/",""), "/^(.{6}).*/","$1")}"
   }
 }
