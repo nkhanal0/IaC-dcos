2927eade0b57d91070ba4cbcf712b24a8d2ba795
Amal <amalv@qburst.com>
2016-11-09 21:49:43 +0530

    Update master.tf

--- master.tf
+++ master.tf
@@ -46,7 +46,9 @@ resource "aws_autoscaling_group" "master" {
     "${aws_alb_target_group.master_https.arn}",
     "${aws_alb_target_group.master_http_80.arn}",
     "${aws_alb_target_group.master_http_8080.arn}",
-    "${aws_alb_target_group.master_http_5050.arn}"
+    "${aws_alb_target_group.master_http_5050.arn}",
+    "${aws_alb_target_group.master_http_8181.arn}",
+    "${aws_alb_target_group.master_http_2181.arn}"
   ]
 
   tag {
@@ -188,6 +190,56 @@ resource "aws_alb_listener" "master_http_5050" {
 
   default_action {
     target_group_arn = "${aws_alb_target_group.master_http_5050.arn}"
+    type = "forward"
+  }
+}
+
+/* HTTP 8181 */
+resource "aws_alb_target_group" "master_http_8181" {
+  name     = "${var.pre_tag}-Master-8181"
+  port     = 8181
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    port = 5050
+    path = "/health"
+  }
+}
+resource "aws_alb_listener" "master_http_8181" {
+  load_balancer_arn = "${aws_alb.master.arn}"
+  port = "8181"
+  protocol = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.master_http_8181.arn}"
+    type = "forward"
+  }
+}
+
+/* HTTP 2181 */
+resource "aws_alb_target_group" "master_http_2181" {
+  name     = "${var.pre_tag}-Master-2181"
+  port     = 2181
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    port = 5050
+    path = "/health"
+  }
+}
+resource "aws_alb_listener" "master_http_2181" {
+  load_balancer_arn = "${aws_alb.master.arn}"
+  port = "2181"
+  protocol = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.master_http_2181.arn}"
     type = "forward"
   }
 }
